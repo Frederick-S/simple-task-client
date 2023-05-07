c5d699c83badce5762073c9a3f3693a73f8a20f4
Frederick-S <mao_xiaodan@hotmail.com>
2023-05-07 14:11:33 +0800

    axios response interceptor

---
+++ src/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import UnauthorizedError from './error/unauthorized-error'
+
+axios.defaults.baseURL = process.env.VUE_APP_API_SERVER_URL
+axios.defaults.withCredentials = true
+
+axios.interceptors.response.use((response) => {
+  const data = response.data
+
+  return data
+}, (error) => {
+  const response = error.response
+  const data = response.data
+  const message = (data && data.error && data.error.message) ? data.error.message : 'Internal server error'
+
+  if (response.status === 401) {
+    return Promise.reject(new UnauthorizedError('Login required'))
+  }
+
+  return Promise.reject(new Error(message))
+})
+
+export default axios
--- src/main.js
+++ src/main.js
@@ -3,12 +3,48 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import Toast, { POSITION } from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+import axios from './axios'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(Toast, {
+  position: POSITION.TOP_CENTER,
+  timeout: 2000
+})
+
+const newVue = () => {
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+try {
+  const me = JSON.parse(localStorage.getItem('me'))
+
+  if (me) {
+    axios.get('/users/me')
+      .then((response) => {
+        store.commit('setMe', response)
+      })
+      .catch((error) => {
+        console.error(error)
+
+        localStorage.removeItem('me')
+        store.commit('setMe', null)
+      })
+      .finally(() => {
+        newVue()
+      })
+  } else {
+    newVue()
+  }
+} catch (error) {
+  console.error(error)
+
+  newVue()
+}
